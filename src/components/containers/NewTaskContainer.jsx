7d20ec5de9fdb5b62bec525207b20cfd4a28ba13
Darren Moy <100505164+darren-moy@users.noreply.github.com>
2024-08-12 16:40:07 -0400

    priority selection for tasks added

--- src/components/containers/NewTaskContainer.jsx
+++ src/components/containers/NewTaskContainer.jsx
@@ -7,14 +7,20 @@ function NewTaskContainer() {
     const handleSubmit = (e) => {
         // Prevent server submission
         e.preventDefault()
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
 
         const { elements } = e.currentTarget
         const userInput = elements.taskContent.value
 
         // Create the task object and dispatch the `addTask` thunk
         const newTask = {
-            content: userInput,
+            content: formJson.taskContent,
+            priority: parseInt(formJson, taskPriority),
         };
+        console.log(newTask);
         dispatch(addTask(newTask));
 
         e.currentTarget.reset();
--- src/store/tasksSlice.js
+++ src/store/tasksSlice.js
@@ -9,33 +9,36 @@ export function tasksReducer(state = initialState, action) {
         return state.filter(task => task.id!==action.payload);
       case 'tasks/taskCreated':
         return [...state, action.payload];
-      case 'task/taskUpdated':
+      case 'tasks/taskUpdated':
         return state.map(task => 
-          task.id===action.payload.id ? action.payload: task);
+          task.id===action.payload.id ? action.payload : task
+        );
       default:
         return state;
     }
-}
+};
 
 //API calls go here
 import axios from "axios";
 //PATH (should be where your server is running)
-const PATH = "http://localhost:5001/api";
+const PATH = "http://localhost:5001/api/tasks";
 
-//Thunk: getting all tasks 
+//Thunks
+
+/* GET ALL TASKS */
 export const fetchTasks = () => async (dispatch) => {
   try {
-    let res = await axios.get(`${PATH}/tasks`);
+    let res = await axios.get(`${PATH}`);
     dispatch({type: 'tasks/tasksLoaded', payload: res.data});
   } catch(err) {
     console.error(err);
   }
 };
 
+/* DELETE TASK */
 export const deleteTask = taskId => async dispatch => {
   try {
-    console.log("Deleting task with id: ", taskId)
-    await axios.delete(`${PATH}/tasks/${taskId}`);
+    await axios.delete(`${PATH}/${taskId}`);
     //delete succesful so change state with dispatch
     dispatch({type: 'tasks/taskDeleted', payload: taskId});
   } catch(err) {
@@ -43,3 +46,24 @@ export const deleteTask = taskId => async dispatch => {
   }
 };
 
+/* ADD TASK */
+export const addTask = task => async (dispatch) => {
+  try {
+    let res = await axios.post(`${PATH}`, task);
+    dispatch({type: 'tasks/taskCreated', payload: res.data});
+    return res.data;
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+/* EDIT TASK */
+export const editTask = task => async dispatch => {
+  try {
+    let res = await axios.put(`${PATH}/${task.id}`, task);
+    //res.data is the updated course
+    dispatch({type: 'tasks/taskUpdated', payload: res.data});
+  } catch(err) {
+    console.error(err);
+  }
+};
